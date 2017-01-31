fcd1a0ae52d70fc7120189e63a577e84fd1063c4
bclouse <bclouse@nevada.unr.edu>
2017-01-31 11:47:17 -0800

    Basic set up until I figure out the normal distribution calculations

--- hw2.cpp
+++ hw2.cpp
@@ -1,3 +1,7 @@
+//
+//	Bryant Clouse
+//	Multi-armed bandit
+
 #include <stdio.h>
 #include <iostream>
 #include <string>
@@ -5,7 +9,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
+#include <random>
 #include <iomanip>
+
+#define ZERO_TO_ONE (float)rand()/RAND_MAX
 
 using namespace std;
 
@@ -13,13 +20,34 @@ using namespace std;
 //	Class definitions
 //===============================================
 
+class MAB {
+	vector<float> mean;
+	vector<float> std;
+	public:
+		MAB();
+		MAB(int);
+		float pull(int);
+};
+
+MAB::MAB() {
+	mean = 1;
+	std = 0.5;
+}
+
+MAB::MAB() {
+
+}
+
+float MAB::pull(int index) {
+
+}
 
 //===============================================
 //	Main Function
 //===============================================
 
-int main(int argv, char * argv[]) {
-
+int main(int argc, char * argv[]) {
+	srand(time(NULL));
 
 	return 0;
 }
