123e704ade2f37df29cebad6ab0e457d8ae07e32
bclouse <bclouse@nevada.unr.edu>
2017-02-02 19:44:56 -0800

    MAB can now be played by a player.

--- hw2.cpp
+++ hw2.cpp
@@ -1,6 +1,7 @@
 //
 //	Bryant Clouse
 //	Multi-armed bandit
+//
 
 #include <stdio.h>
 #include <iostream>
@@ -11,8 +12,10 @@
 #include <assert.h>
 #include <random>
 #include <iomanip>
+#include <cmath>
 
-#define ZERO_TO_ONE (float)rand()/RAND_MAX
+#define PI_ (double) 3.14159265358979323846
+#define ZERO_TO_ONE (double)rand()/RAND_MAX
 
 using namespace std;
 
@@ -21,25 +24,41 @@ using namespace std;
 //===============================================
 
 class MAB {
-	vector<float> mean;
-	vector<float> std;
+	double mean;
+	double std;
 	public:
-		MAB();
-		MAB(int);
-		float pull(int);
+		MAB(bool);
+		double pull();
+		void set_values(bool);
+		void display();
 };
 
-MAB::MAB() {
-	mean = 1;
-	std = 0.5;
+void MAB::set_values(bool randomize) {
+	if (randomize) {
+		mean = (ZERO_TO_ONE-0.5)*50;
+		std = ZERO_TO_ONE*25;
+	} else {
+		mean = 0;
+		std = 1;
+	}
 }
 
-MAB::MAB() {
-
+MAB::MAB(bool randomize) {
+	set_values(randomize);
 }
 
-float MAB::pull(int index) {
 
+double MAB::pull() {
+	double output;
+	double U1 = ZERO_TO_ONE, U2 = ZERO_TO_ONE;
+	output = (sqrt((double)-2*log(U1))*sin(2*PI_*U2))*std+mean;
+	return output;
+}
+
+void MAB::display() {
+	cout.fill(' '); cout.width(12);
+	cout << "mean = " << mean << '\t';
+	cout << "std  = " << std << "\n\n";
 }
 
 //===============================================
@@ -47,7 +66,29 @@ float MAB::pull(int index) {
 //===============================================
 
 int main(int argc, char * argv[]) {
+	MAB dummy (false);
+	vector<MAB> MAB_list;
+	int input;
+
 	srand(time(NULL));
+	for (int i = 0; i < 3; i++) {
+		dummy.set_values(true);
+		MAB_list.push_back(dummy);
+	}
+
+	while (1) {
+		cout << ">> ";
+		cin >> input;
+		if (!(input >= 1 && input <= MAB_list.size())) {
+			cout << "NOT IN RANGE!\nENDING PROGRAM!\n\n";
+			break;
+		}
+		cout << "You get: " << MAB_list.at(input-1).pull() << endl;
+	}
+	cout << "Here are the results:\n";
+	for (int i = 0; i < 3; i++) {
+		MAB_list.at(i).display();
+	}
 
 	return 0;
 }
